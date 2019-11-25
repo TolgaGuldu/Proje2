df469afa92a0b6d47be4dd9a0bb39cadb96658f6
TolgaGuldu <>
2019-11-26 00:40:45 +0300

    son
    son

---
+++ counquiz/app/src/main/java/com/example/counquiz/Database.java
@@ -0,0 +1,33 @@
+package com.example.counquiz;
+
+public class Database {
+    Integer[] flags = {
+            R.drawable.Azerbaijan,
+            R.drawable.China,
+            R.drawable.Germany,
+            R.drawable.KKTC,
+            R.drawable.Turkey,
+    };
+    String[] answers = {
+            "Azerbaijan",
+            "China",
+            "Germany",
+            "KKTC",
+            "Turkey",
+    };
+
+    String[] capitals = {
+            "Bakü",
+            "Pekin",
+            "Berlin(Kreuzberg)",
+            "Girne",
+           "Ankara",
+    };
+    String[] country = {
+            "Azerbaijan",
+            "China",
+            "Germany",
+            "KKTC",
+            "Turkey",
+    };
+}
---
+++ counquiz/app/src/main/java/com/example/counquiz/MainActivity.java
@@ -0,0 +1,224 @@
+package com.example.counquiz;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.List;
+
+public class MainActivity extends Activity {
+    Button a_choice, b_choice, c_choice, d_choice;
+
+    ImageView flag_pic;
+
+    List<CountryItems> list;
+
+    Random r;
+
+    int turn = 1;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        r = new Random();
+
+        flag_pic = (ImageView) findViewById((R.id.flag_pic));
+        a_choice = (Button) findViewById((R.id.a_choice));
+        b_choice = (Button) findViewById((R.id.b_choice));
+        c_choice = (Button) findViewById((R.id.c_choice));
+        d_choice = (Button) findViewById((R.id.d_choice));
+
+        list = new ArrayList<>();
+
+        for (int i = 0; 1 < new Database().answers.length; i++) {
+            list.add(new CountryItems(new Database().answers[1], new Database().flags[1]));
+
+        }
+
+
+        Collections.shuffle(list);
+
+        newQuestion(turn);
+
+        a_choice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (a_choice.getText().toString().equalsIgnoreCase(list.get(turn - 1).getName())) {
+                    Toast.makeText(MainActivity.this, "Correct", Toast.LENGTH_LONG).show();
+                    if (turn < list.size()) {
+                        turn++;
+                        newQuestion(turn);
+                    } else {
+                        Toast.makeText(MainActivity.this, "quiz finsihed", Toast.LENGTH_LONG).show();
+                        Intent intent=new Intent(MainActivity.this,MainActivity2.class);
+                        startActivity(intent);
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_LONG).show();
+                    if (turn < list.size()) {
+                        turn++;
+                        newQuestion(turn);
+                    } else {
+                        Toast.makeText(MainActivity.this, "finished", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+        b_choice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (b_choice.getText().toString().equalsIgnoreCase(list.get(turn - 1).getName())) {
+                    Toast.makeText(MainActivity.this, "Correct", Toast.LENGTH_LONG).show();
+                    if (turn < list.size()) {
+                        turn++;
+                        newQuestion(turn);
+                    } else {
+                        Toast.makeText(MainActivity.this, "quiz finsihed", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_LONG).show();
+                    if (turn < list.size()) {
+                        turn++;
+                        newQuestion(turn);
+                    } else {
+                        Toast.makeText(MainActivity.this, "finished", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+        c_choice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (c_choice.getText().toString().equalsIgnoreCase(list.get(turn - 1).getName())) {
+                    Toast.makeText(MainActivity.this, "Correct", Toast.LENGTH_LONG).show();
+                    if (turn < list.size()) {
+                        turn++;
+                        newQuestion(turn);
+                    } else {
+                        Toast.makeText(MainActivity.this, "quiz finsihed", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_LONG).show();
+                    if (turn < list.size()) {
+                        turn++;
+                        newQuestion(turn);
+                    } else {
+                        Toast.makeText(MainActivity.this, "finished", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+        d_choice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (d_choice.getText().toString().equalsIgnoreCase(list.get(turn - 1).getName())) {
+                    Toast.makeText(MainActivity.this, "Correct", Toast.LENGTH_LONG).show();
+                    if (turn < list.size()) {
+                        turn++;
+                        newQuestion(turn);
+                    } else {
+                        Toast.makeText(MainActivity.this, "quiz finsihed", Toast.LENGTH_LONG).show();
+                    }
+                } else {
+                    Toast.makeText(MainActivity.this, "wrong", Toast.LENGTH_LONG).show();
+                    if (turn < list.size()) {
+                        turn++;
+                        newQuestion(turn);
+                    } else {
+                        Toast.makeText(MainActivity.this, "finished", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+
+
+    private void newQuestion(int number){
+        flag_pic.setImageResource(list.get(number-1).getImage());
+        int correct_answer = r.nextInt(4 +1);
+
+        int firstButton= number -1;
+        int secondButton;
+        int thirdButton;
+        int forthButton;
+
+        switch (correct_answer){
+            case 1:
+            a_choice.setText(list.get(firstButton).getName());
+
+            do{
+                secondButton = r.nextInt(list.size());
+            } while(secondButton == firstButton);
+            do{
+                    thirdButton = r.nextInt(list.size());
+            } while(thirdButton == secondButton || thirdButton == firstButton);
+            do{
+                    forthButton = r.nextInt(list.size());
+            } while(forthButton == thirdButton || forthButton ==  secondButton  || forthButton == firstButton);
+
+            b_choice.setText(list.get(secondButton).getName());
+            c_choice.setText(list.get(thirdButton).getName());
+            d_choice.setText(list.get(forthButton).getName());
+            case 2:
+                b_choice.setText(list.get(firstButton).getName());
+
+                do{
+                    secondButton = r.nextInt(list.size());
+                } while(secondButton == firstButton);
+                do{
+                    thirdButton = r.nextInt(list.size());
+                } while(thirdButton == secondButton || thirdButton == firstButton);
+                do{
+                    forthButton = r.nextInt(list.size());
+                } while(forthButton == thirdButton || forthButton ==  secondButton  || forthButton == firstButton);
+
+                a_choice.setText(list.get(secondButton).getName());
+                c_choice.setText(list.get(thirdButton).getName());
+                d_choice.setText(list.get(forthButton).getName());
+            case 3:
+                c_choice.setText(list.get(firstButton).getName());
+
+                do{
+                    secondButton = r.nextInt(list.size());
+                } while(secondButton == firstButton);
+                do{
+                    thirdButton = r.nextInt(list.size());
+                } while(thirdButton == secondButton || thirdButton == firstButton);
+                do{
+                    forthButton = r.nextInt(list.size());
+                } while(forthButton == thirdButton || forthButton ==  secondButton  || forthButton == firstButton);
+
+                a_choice.setText(list.get(secondButton).getName());
+                b_choice.setText(list.get(thirdButton).getName());
+                d_choice.setText(list.get(forthButton).getName());
+            case 4:
+                d_choice.setText(list.get(firstButton).getName());
+
+                do{
+                    secondButton = r.nextInt(list.size());
+                } while(secondButton == firstButton);
+                do{
+                    thirdButton = r.nextInt(list.size());
+                } while(thirdButton == secondButton || thirdButton == firstButton);
+                do{
+                    forthButton = r.nextInt(list.size());
+                } while(forthButton == thirdButton || forthButton ==  secondButton  || forthButton == firstButton);
+
+                a_choice.setText(list.get(secondButton).getName());
+                b_choice.setText(list.get(thirdButton).getName());
+                c_choice.setText(list.get(forthButton).getName());
+
+        }
+
+    }
+
+
+}
